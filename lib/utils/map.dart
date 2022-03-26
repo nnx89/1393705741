d4924a57fa64ab5529d579fb5a96fc1b6d0f010a
NightOwl <midnightow1@outlook.com>
2022-03-26 22:02:34 +0800

    Add basic Map Overlay layer

--- lib/utils/map.dart
+++ lib/utils/map.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart';
 import 'package:flutter_baidu_mapapi_map/flutter_baidu_mapapi_map.dart';
 
-void setAK() {
+void initMapSDK() {
   if (Platform.isIOS) {
     BMFMapSDK.setApiKeyAndCoordType(
         'SyaWCruyXEYo48uB9BqDfaCIWAvZwrQn', BMF_COORD_TYPE.BD09LL);
@@ -15,34 +15,103 @@ void setAK() {
   }
 }
 
-class MapManager {
-  static BMFMapOptions mapOptions = BMFMapOptions(
-      center: BMFCoordinate(39.917215, 116.380341),
-      zoomLevel: 12,
-      mapPadding: BMFEdgeInsets(left: 30, top: 0, right: 30, bottom: 0));
+class MapWidget extends StatefulWidget {
+  const MapWidget({Key? key, required this.mapOptions, this.overlays})
+      : super(key: key);
 
-  static BMFMapController? mapContoller;
+  final BMFMapOptions mapOptions;
+  final List<MapOverlay>? overlays;
 
-  static Widget createMap(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    return SizedBox(
-      height: screenSize.height,
-      width: screenSize.width,
-      child: BMFMapWidget(
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  List<MapOverlay> visibleOverlays = [];
+  BMFMapStatus? mapStatus;
+
+  void onDrawMapFrame(BMFMapStatus mapStatus) {
+    assert(mapStatus.coordinateBounds != null);
+
+    if (widget.overlays == null) {
+      return;
+    }
+    var visibleOverlays = <MapOverlay>[];
+    for (MapOverlay overlay in widget.overlays!) {
+      if (mapStatus.coordinateBounds!.southwest.latitude <=
+              overlay.coordinate.latitude &&
+          overlay.coordinate.latitude <=
+              mapStatus.coordinateBounds!.northeast.latitude &&
+          mapStatus.coordinateBounds!.southwest.longitude <=
+              overlay.coordinate.longitude &&
+          overlay.coordinate.longitude <=
+              mapStatus.coordinateBounds!.northeast.longitude) {
+        visibleOverlays.add(overlay);
+      }
+    }
+    this.mapStatus = mapStatus;
+    setState(() {
+      this.visibleOverlays = visibleOverlays;
+    });
+    // print(
+    //     "${mapStatus.targetGeoPt?.latitude}    ${mapStatus.targetGeoPt?.longitude}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var children = [
+      BMFMapWidget(
         onBMFMapCreated: (controller) {
-          // onBMFMapCreated(controller);
+          controller.setMapOnDrawMapFrameCallback(callback: onDrawMapFrame);
         },
-        mapOptions: mapOptions,
+        mapOptions: widget.mapOptions,
       ),
+      Positioned(
+        child: Container(
+          height: 50,
+          width: 50,
+          color: Colors.green,
+        ),
+        right: 50,
+        bottom: 50,
+      )
+    ];
+
+    var parentSize = MediaQuery.of(context).size;
+    if (mapStatus != null) {
+      assert(mapStatus!.coordinateBounds != null);
+      double latitudeHeight = mapStatus!.coordinateBounds!.northeast.latitude -
+          mapStatus!.coordinateBounds!.southwest.latitude;
+      double longitudeWidth = mapStatus!.coordinateBounds!.northeast.longitude -
+          mapStatus!.coordinateBounds!.southwest.longitude;
+      double xFactor = parentSize.width / longitudeWidth;
+      double yFactor = parentSize.height / latitudeHeight;
+      children.addAll(visibleOverlays.map((overlay) => Positioned(
+            child: overlay,
+            left: (overlay.coordinate.longitude -
+                    mapStatus!.coordinateBounds!.southwest.longitude) *
+                xFactor,
+            bottom: (overlay.coordinate.latitude -
+                    mapStatus!.coordinateBounds!.southwest.latitude) *
+                yFactor,
+          )));
+    }
+
+    return Stack(
+      children: children,
     );
   }
 }
 
-class MapWidget extends StatelessWidget {
-  const MapWidget({Key? key}) : super(key: key);
+class MapOverlay extends StatelessWidget {
+  const MapOverlay({Key? key, required this.coordinate, required this.child})
+      : super(key: key);
+
+  final BMFCoordinate coordinate;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return MapManager.createMap(context);
+    return child;
   }
 }
