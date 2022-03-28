6d9361973d96c5d49740378c0c5e543becdd99a4
yifei luo <sophiefeifeifeiya@gmail.com>
2022-03-28 14:06:56 +0800

    commit first bubble

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+
+
+import 'dart:io';
+
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+// Sets a platform override for desktop to avoid exceptions. See
+// https://flutter.dev/desktop#target-platform-override for more info.
+void _enablePlatformOverrideForDesktop() {
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
+void main() {
+  _enablePlatformOverrideForDesktop();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'bubble',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _animate = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("bubble"),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: const AvatarGlow(
+                startDelay: Duration(milliseconds: 1000),
+                glowColor: Colors.blue,
+                //边框大小
+                endRadius: 120.0,
+                duration: Duration(milliseconds: 2000),
+                repeat: true,
+                showTwoGlows: true,
+                repeatPauseDuration: Duration(milliseconds: 100),
+                child: Material(
+                  elevation: 8.0,
+                  shape: CircleBorder(),
+                  color: Colors.transparent,
+                  child: CircleAvatar(
+                    //picture
+                    backgroundImage: AssetImage('images/avatar.png'),
+                    radius: 70.0,
+                  ),
+                ),
+                shape: BoxShape.circle,
+                animate: true,
+                curve: Curves.fastOutSlowIn,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _animate = !_animate;
+          });
+        },
+      ),
+    );
+  }
+}
