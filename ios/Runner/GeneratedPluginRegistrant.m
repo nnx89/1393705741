9c7297900fee2f012f956ae6d3633a9ba4d398a6
rexjz <robbort.xu@gmail.com>
2022-04-11 14:07:29 +0800

    brfore integrate

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<amap_flutter_map/AMapFlutterMapPlugin.h>)
+#import <amap_flutter_map/AMapFlutterMapPlugin.h>
+#else
+@import amap_flutter_map;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AMapFlutterMapPlugin registerWithRegistrar:[registry registrarForPlugin:@"AMapFlutterMapPlugin"]];
+}
+
+@end
