0f27048b11db4470381cd0d314270204e4999120
yifei luo <sophiefeifeifeiya@gmail.com>
2022-04-11 13:00:36 +0800

    profile: both editing and displaying profile

---
+++ README.md
@@ -0,0 +1,7 @@
+The profile
+
+lib/utils/user+preferences.dart: edit the default profile
+
+lib/page/edit_profile_page.dart: edit the information
+
+lib/page/profile_page.dart: display the information
---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,14 @@
+import 'package:profile_formal/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+    'images/1.jpg',
+    name: 'Kaedehara Kazuha',
+    email: 'wanye@gmail.com',
+    about:
+        'ok.',
+    isDarkMode: true,
+  );
+}
+
